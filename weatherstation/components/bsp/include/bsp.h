63737e3d88938d3de49230d1cb540e36f509be12
Erik <erikmiyoshi@hotmail.com>
2026-02-21 18:37:38 -0300

    remove old dependency, rename bsp files

--- weatherstation/components/bsp/include/bsp.h
+++ weatherstation/components/bsp/include/bsp.h
@@ -1,16 +1,7 @@
 #ifndef BSP_H
 #define BSP_H
 
-#include "driver/gpio.h"
-
-#define N_BUTTONS 3
-
-typedef enum {
-    ID_BUTTON_LEFT = 0,
-    ID_BUTTON_CENTER,
-    ID_BUTTON_RIGHT,
-} button_id_t;
-
-gpio_num_t button_get_gpio(button_id_t id);
+#include "bsp_button.h"
+#include "bsp_i2c.h"
 
 #endif
---
+++ weatherstation/components/bsp/include/bsp_i2c.h
@@ -0,0 +1,9 @@
+#ifndef BSP_I2C_H
+#define BSP_I2C_H
+
+#include "driver/gpio.h"
+
+#define I2C0_SDA 21
+#define I2C0_SCL 22
+
+#endif
